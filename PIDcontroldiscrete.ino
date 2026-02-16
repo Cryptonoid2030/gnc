999e802e8ed3cd9fad8deb5fd0a557c19a450fad
Cryptonoid2030 <137273973+Cryptonoid2030@users.noreply.github.com>
2026-02-16 13:20:03 -0500

    Refactor PID control parameters and calculations

--- PIDcontroldiscrete.ino
+++ PIDcontroldiscrete.ino
@@ -4,19 +4,19 @@
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-float reference = 0; // euler angle for reference
-float samplePer = 0.06; // select a sampling period consistent > clk & sensor sampling
+float reference = 0; 
+float samplePer = 0.06; 
 
-// tuneable values
+
 float kp = 1.0;
 float kd = 1.0;
 
-float threshold = 5; // sensitivity before controller action
-float fc = 5; // 5 hz cutoff low pass filter
-// float alpha = 0.2; // hard coded low pass filter
-float alpha = samplePer / (samplePer + 1/(2*PI*fc)); // automatic low pass filter based on cutoff frequency
+float threshold = 5; 
+float fc = 5; 
+// float alpha = 0.2; 
+float alpha = samplePer / (samplePer + 1/(2*PI*fc)); 
 
-// loop variables (no need to change)
+
 float errors[3] = {0, 0, 0};
 int counter = 0; 
 
@@ -51,8 +51,8 @@ void loop(void) {
     updateError(error, errors);
 
     // now define the control action
-    float u = alpha * (kp * errors[0] + (kd / samplePer) * (errors[0] - errors[1])) + // current error
-              (1 - alpha) * (kp * errors[1] + (kd / samplePer) * (errors[1] - errors[2])); // previous error
+    float u = alpha * (kp * errors[0] + (kd / samplePer) * (errors[0] - errors[1])) +
+              (1 - alpha) * (kp * errors[1] + (kd / samplePer) * (errors[1] - errors[2]));
 
     if (abs(u) > threshold) {
       if (u > 0) {
@@ -62,7 +62,6 @@ void loop(void) {
       }
     }
   } else {
-    // error buffer is empty. start filling now
     errors[2 - counter] = error;
     counter++;
   }
